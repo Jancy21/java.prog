2b7282455c05bf3eef3b4c3bff53f9a64279fa98
Asus <Asus@ASUSjanu06>
2024-06-27 12:13:52 +0530

    this is a DataType assign of theBasic java program.

---
+++ javademo/src/day2/DataTypes.java
@@ -0,0 +1,24 @@
+package day2;
+
+public class DataTypes {
+
+	public static void main(String[] args) 
+	{
+		int values=9/2;
+		float value1=101/61;
+		double value2=10d/6d;
+		float value3=5/2.5f;
+		System.out.println("Value1="+values);
+		System.out.println("values2="+value1);
+		System.out.println("values3="+value2);
+		System.out.println("Values4="+value3);
+		
+		int marker=512;
+		
+		double percentage= marker*0.46f;
+		System.out.println("percentage:"+percentage);
+		
+
+	}
+
+}
