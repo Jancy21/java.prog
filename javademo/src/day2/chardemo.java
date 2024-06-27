6cca0b1e07fb83b083a1d02e29a6b73471bcbbb4
Asus <Asus@ASUSjanu06>
2024-06-27 11:42:27 +0530

    This is a char assignment.

---
+++ javademo/src/day2/chardemo.java
@@ -0,0 +1,23 @@
+package day2;
+
+public class chardemo 
+{
+	public static void main(string[] args) 
+	{
+		char ch='a';
+		System.out.println(ch);
+		
+		char ch1=65;
+		System.out.println(ch1);
+		
+		char ch2='\u00a7';
+		System.out.println(ch2);
+		
+		char ch3='\u20ac';
+		System.out.println(ch3);
+		
+		int a='A';
+		System.out.println(a);		
+		
+	}
+}
